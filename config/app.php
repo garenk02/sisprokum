a703c2ebc84e55ffa372f2001d0fc91d4fc90691
Trenggana Natadirja <trenggana.natadirja@klb.co.id>
2020-04-11 14:29:19 +0700

    Customer export to excel file

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://sisprokum.herokuapp.com'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
